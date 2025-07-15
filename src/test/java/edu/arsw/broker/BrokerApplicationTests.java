7c2674750466a9bfa9cdd90cf6277abba0215d7a
Andr3xDev <achavarropp@gmail.com>
2025-07-15 09:56:25 -0500

    add: initial project

---
+++ src/test/java/edu/arsw/broker/BrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.arsw.broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
